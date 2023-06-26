e157356de52d810b5468757d693fcacb86af06ea
Frederick Morrison <frederick.morrison@outlook.com>
2023-06-26 11:07:11 -0600

    generic starter code

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+
+@staticmethod
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(f'Python version {get_python_version()}')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Python Forest of Trees
+## References
+* [Youtube tutorial](https://www.youtube.com/watch?v=-LsuiVGO-88)
