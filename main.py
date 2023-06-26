bd297986303195551eec6bbae81f35582fc7cacd
Frederick Morrison <frederick.morrison@outlook.com>
2023-06-26 11:32:16 -0600

    add initial blank background screen

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import sys
 
+import cv2 as cv
+import numpy as np
+
+# general parameters
+width = 900
+height = 600
+n_trees = 30
+ground_level = height - 100
+
+# colours
+green, light_green, brown = (40, 185, 40), (25, 220, 0), (30, 65, 155)
+
+# blank image
+bg = np.zeros((height, width, 3), dtype=np.uint8)
+
 
 @staticmethod
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(f'Python version {get_python_version()}')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # draw background
+    cv.rectangle(bg, (width, 0), (0, ground_level), (255, 225, 95), -1)
+    cv.rectangle(bg, (width, ground_level), (0, height), green, -1)
+
+    # ***************
+    # YOUR CODE GOES HERE
+    # ***************
+
+    # display image
+    cv.imshow('forest of objects', bg)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Python Forest of Trees
 ## References
 * [Youtube tutorial](https://www.youtube.com/watch?v=-LsuiVGO-88)
+* [OpenCV-Python](https://pypi.org/project/opencv-python/)
+* [OpenCV-Pyton docs](https://docs.opencv.org/4.7.0/)
+* [OpenCV3](https://docs.opencv.org/3.0-beta/index.html)
+## Issues 
+* ```import cv2 as cv``` fails during pip install on 2023-06-26
+    solution: ```pip install opencv-python``` which installs version 4.7
+* 
